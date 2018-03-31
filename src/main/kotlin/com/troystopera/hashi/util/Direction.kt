a95141348a79bb52681e25977ef0d055fa064cb7
Troy Stopera <troystopera@gmail.com>
2018-03-31 03:17:46 -0400

    Improved Direction class

--- src/main/kotlin/com/troystopera/hashi/util/Direction.kt
+++ src/main/kotlin/com/troystopera/hashi/util/Direction.kt
@@ -2,16 +2,16 @@ package com.troystopera.hashi.util
 
 import com.troystopera.hashi.gen.Random
 
-enum class Direction {
+enum class Direction(private val rowChange: Int, private val columnChange: Int) {
 
-    UP, DOWN, LEFT, RIGHT;
+    UP(-1, 0),
+    DOWN(1, 0),
+    LEFT(0, -1),
+    RIGHT(0, 1);
 
-    fun translate(coordinate: Coordinate, dist: Int) = if (dist >= 0) when (this) {
-        UP -> Coordinate(coordinate.row - dist, coordinate.col)
-        DOWN -> Coordinate(coordinate.row + dist, coordinate.col)
-        LEFT -> Coordinate(coordinate.row, coordinate.col - dist)
-        RIGHT -> Coordinate(coordinate.row, coordinate.col + dist)
-    } else throw IllegalArgumentException("Distance must be non-negative")
+    fun translate(coordinate: Coordinate, dist: Int): Coordinate {
+        return Coordinate(coordinate.row + (rowChange * dist), coordinate.col + (columnChange * dist))
+    }
 
     fun opposite() = when (this) {
         UP -> DOWN
@@ -25,14 +25,13 @@ enum class Direction {
         private val rOrder = arrayOf(UP, DOWN, LEFT, RIGHT)
 
         fun randomOrder(random: Random): MutableList<Direction> {
-            //shuffle current oder
-            for (i in 0..rOrder.size) {
+            //shuffle current order
+            for (i in 0 until rOrder.size) {
                 val swapIndex = random.nextInt(rOrder.size)
                 val temp = rOrder[swapIndex]
                 rOrder[swapIndex] = rOrder[i]
                 rOrder[i] = temp
             }
-
             return rOrder.toMutableList()
         }
 
