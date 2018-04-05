2718b19983b818082d780f3d9e3e6f0daefeb87a
Troy Stopera <troystopera@gmail.com>
2018-04-04 21:10:47 -0400

    Added puzzle and generation constraints

---
+++ src/main/kotlin/com/troystopera/hashi/Constraints.kt
@@ -0,0 +1,16 @@
+package com.troystopera.hashi
+
+object Constraints {
+
+    const val MIN_BRIDGE_WIDTH = 1
+    const val MAX_BRIDGE_WIDTH = 16
+
+    const val MIN_PUZZLE_WIDTH = 5
+    const val MIN_PUZZLE_HEIGHT = 5
+    const val MAX_PUZZLE_WIDTH = 256
+    const val MAX_PUZZLE_HEIGHT = 256
+
+    const val MIN_BRIDGE_DENSITY = 0.0
+    const val MAX_BRIDGE_DENSITY = 1.0
+
+}
--- src/main/kotlin/com/troystopera/hashi/gen/Generation.kt
+++ src/main/kotlin/com/troystopera/hashi/gen/Generation.kt
@@ -1,5 +1,6 @@
 package com.troystopera.hashi.gen
 
+import com.troystopera.hashi.Constraints
 import com.troystopera.hashi.HashiPuzzle
 import com.troystopera.hashi.util.Grid
 
@@ -11,7 +12,7 @@ class Generator(val options: GenerationOptions) {
         val initialNode = GenNode(0, random.nextNormalInt(options.height), random.nextNormalInt(options.width), random)
         val spreadAlg = options.algorithm.getInstance(grid, options, random, initialNode)
 
-        while (spreadAlg.nodeCount() < options.targetNodeCount && spreadAlg.canSpread())
+        while (spreadAlg.nodeCount() < options.nodeCount && spreadAlg.canSpread())
             spreadAlg.spread()
 
         return HashiPuzzle(grid)
@@ -19,13 +20,47 @@ class Generator(val options: GenerationOptions) {
 
 }
 
-data class GenerationOptions(
-        val height: Int = 7,
-        val width: Int = 7,
-        val targetNodeCount: Int = 12,
-        val maxBridgeWidth: Int = 2,
+class GenerationOptions(
+        height: Int = 7,
+        width: Int = 7,
+        nodeCount: Int = 12,
+        bridgeDensity: Double = 0.5,
+        maxBridgeWidth: Int = 2,
         val algorithm: GenerationAlgorithm
-)
+) {
+
+    val height: Int = when {
+        height < Constraints.MIN_PUZZLE_HEIGHT -> Constraints.MIN_PUZZLE_HEIGHT
+        height > Constraints.MAX_PUZZLE_HEIGHT -> Constraints.MAX_PUZZLE_HEIGHT
+        else -> height
+    }
+
+    val width: Int = when {
+        height < Constraints.MIN_PUZZLE_WIDTH -> Constraints.MIN_PUZZLE_WIDTH
+        height > Constraints.MAX_PUZZLE_WIDTH -> Constraints.MAX_PUZZLE_WIDTH
+        else -> height
+    }
+
+    val nodeCount: Int = when {
+        nodeCount > ((height * width) * 0.5) -> ((height * width) * 0.5).toInt()
+        else -> nodeCount
+    }
+
+    val bridgeDensity: Double = when {
+        bridgeDensity < Constraints.MIN_BRIDGE_DENSITY -> Constraints.MIN_BRIDGE_DENSITY
+        bridgeDensity > Constraints.MAX_BRIDGE_DENSITY -> Constraints.MAX_BRIDGE_DENSITY
+        else -> bridgeDensity
+    }
+
+    val maxBridgeWidth: Int = when {
+        maxBridgeWidth < Constraints.MIN_BRIDGE_WIDTH -> Constraints.MIN_BRIDGE_WIDTH
+        maxBridgeWidth > Constraints.MAX_BRIDGE_WIDTH -> Constraints.MAX_BRIDGE_WIDTH
+        else -> maxBridgeWidth
+    }
+
+    internal val bridgeSpreadProb: Double = 0.5 + (0.5 * bridgeDensity)
+
+}
 
 enum class GenerationAlgorithm {
 
